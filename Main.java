c465acd14b076612b5890bd8e976eccd5dd7555c
ankitadubey2004 <155213532+ankitadubey2004@users.noreply.github.com>
2024-04-30 09:35:29 +0530

    ADDED

--- Main.java
+++ Main.java
@@ -6,3 +6,20 @@ interface utility {
 
     public void get_details(){;
 }
+
+public class Main{
+
+    static void main_menu(){
+        System.out.println();
+        System.out.println("================== *** WELCOME TO SHOWROOM MANAGEMENT SYSTEM *** =================");
+        System.out.println();
+        System.out.println("============================ *** ENTER YOUR CHOICE *** ==============================");
+        System.out.println();
+        System.out.println("1].ADD SHOWROOMS \t\t\t 2].ADD EMPLOYEES \t\t\t 3].ADD CARS");
+        System.out.println();
+        System.out.println("4].GET SHOWROOMS \t\t\t 5].GET EMPLOYEES \t\t\t 6].GET CARS");
+        System.out.println();
+        System.out.println("============================ *** ENTER 0 TO EXIT *** ==============================");
+
+    }
+}
